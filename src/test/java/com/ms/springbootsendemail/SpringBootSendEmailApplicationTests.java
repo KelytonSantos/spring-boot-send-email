a839f8787da9fce06863596002648aaace213f3b
Kelyton Santos <kelytonlucas@gmail.com>
2022-09-10 17:47:55 -0300

    repositorio criado

---
+++ src/test/java/com/ms/springbootsendemail/SpringBootSendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.springbootsendemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSendEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
